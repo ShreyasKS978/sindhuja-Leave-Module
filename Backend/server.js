ff2f60d1665cf067c1afbe4d23716e5768e7b15e
Shreyas <shreyasks978@gmail.com>
2025-06-19 17:41:57 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -138,5 +138,5 @@ app.delete('/api/leave-requests', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://13.201.194.132:${port}`);
+    console.log(`Server running at http://13.126.5.224:${port}`);
 });
